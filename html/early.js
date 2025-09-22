644e861bf6d6d8000c6bb3480fa66f82fadf6006
spattenjetnet <spatten@jetnet.com>
2025-09-22 09:52:44 -0600

    Removed dynGlobeRate

--- html/early.js
+++ html/early.js
@@ -521,16 +521,16 @@ if (!heatmap) {
 
 }
 
-function globeRateUpdate() {
-    if (aggregator) {
-        dynGlobeRate = !0;
-        const cookieExp = getCookie("adsbx_sid").split("_")[0], ts = (new Date).getTime();
-        (!cookieExp || cookieExp < ts + 36e5) && setCookie("adsbx_sid", ts + 1728e5 + "_" + Math.random().toString(36).substring(2, 15), 2)
-    }
-    return dynGlobeRate ? jQuery.ajax({url: "/globeRates.json", cache: !1, dataType: "json"}).done((function (data) {
-        null != data.simload && (globeSimLoad = data.simload), null != data.refresh && globeIndex && (RefreshInterval = data.refresh)
-    })) : jQuery.Deferred().resolve()
-}
+// function globeRateUpdate() {
+//     if (aggregator) {
+//         dynGlobeRate = !0;
+//         const cookieExp = getCookie("adsbx_sid").split("_")[0], ts = (new Date).getTime();
+//         (!cookieExp || cookieExp < ts + 36e5) && setCookie("adsbx_sid", ts + 1728e5 + "_" + Math.random().toString(36).substring(2, 15), 2)
+//     }
+//     return dynGlobeRate ? jQuery.ajax({url: "/globeRates.json", cache: !1, dataType: "json"}).done((function (data) {
+//         null != data.simload && (globeSimLoad = data.simload), null != data.refresh && globeIndex && (RefreshInterval = data.refresh)
+//     })) : jQuery.Deferred().resolve()
+// }
 
 if (uuid != null) {
     receiverJson = null;
@@ -721,7 +721,7 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-globeRateUpdate();
+// globeRateUpdate();
 
 const toggles = {};
 
